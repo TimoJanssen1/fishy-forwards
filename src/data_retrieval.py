fb36daa3fd3f1cc8f98299035200e4eb0b826ddd
Paul <paul19wen@gmail.com>
2026-02-18 22:36:12 +0100

    added data

---
+++ src/data_retrieval.py
@@ -0,0 +1,12 @@
+class DataGetter:
+
+    def __init__(self):
+        ...
+
+    def get_forward_prices(self):
+        # https://fishpool.eu/forward-prices/
+        ...
+
+    def get_price_history(self):
+        # https://fishpool.eu/price-history/
+        ...
